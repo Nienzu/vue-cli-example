a3f7f4b6d34e3f9b649c341abdd8a9edf3e3c456
nienzu <ibqqz0602@gmail.com>
2018-06-11 18:35:53 +0800

    leaflet example

---
+++ src/components/map.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="map">
+
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'map'
+    }
+</script>
+<style scoped>
+#map { height: 180px; }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import markdown from '@/components/markdown'
+import map from '@/components/map'
 Vue.use(Router)
 
 export default new Router({
@@ -16,6 +17,11 @@ export default new Router({
       path:'/markdown',
       name:'markdown',
       component:markdown
+    },
+    {
+      path:'/map',
+      name:'map',
+      component:map
     }
   ]
 })
